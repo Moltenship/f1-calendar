b309fba38351b507e1c8724f528450463e00727d
Timofey Maximov <moltenship7750@gmail.com>
2024-08-04 16:28:40 +0600

    Add dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import "@fontsource/inter"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { ThemeProvider } from "./components/ui/theme-provider.tsx"
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -17,9 +18,11 @@ const queryClient = new QueryClient({
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<App />
-			<ReactQueryDevtools />
-		</QueryClientProvider>
+		<ThemeProvider >
+			<QueryClientProvider client={queryClient}>
+				<App />
+				<ReactQueryDevtools />
+			</QueryClientProvider>
+		</ThemeProvider>
 	</React.StrictMode>,
 )
